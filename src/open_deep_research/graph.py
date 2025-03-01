4bc3aad186d1306656ee5e9befd1ca7e5805c683
codeliveyou <babyskydragon515@gmail.com>
2025-02-28 21:58:02 -0600

    change prompts simply

--- src/open_deep_research/graph.py
+++ src/open_deep_research/graph.py
@@ -16,7 +16,7 @@ from typing import Dict, Any
 import uuid
 
 from .state import ReportStateInput, ReportStateOutput, Sections, Section, ReportState, SectionState, SectionOutputState, Queries, Feedback
-from .prompts import get_report_planner_query_writer_instructions, get_report_planner_instructions, get_query_writer_instructions, get_section_grader_instructions, final_section_writer_instructions, get_section_writer_instructions
+from .prompts import report_planner_query_writer_instructions,   section_grader_instructions, final_section_writer_instructions, report_planner_instructions, query_writer_instructions, section_writer_instructions
 from .configuration import Configuration
 from .utils import tavily_search_async, deduplicate_and_format_sources, format_sections, perplexity_search
 import logging
@@ -95,7 +95,7 @@ async def generate_report_plan(state: ReportState, config: RunnableConfig):
         structured_llm = writer_model.with_structured_output(Queries)
 
         # Format system instructions
-        system_instructions_query =  get_report_planner_query_writer_instructions(report_type).format(topic=topic, report_organization=report_structure, number_of_queries=number_of_queries)
+        system_instructions_query =  report_planner_query_writer_instructions.format(topic=topic, report_organization=report_structure, number_of_queries=number_of_queries)
 
         # Generate queries  
         results = structured_llm.invoke([SystemMessage(content=system_instructions_query)] + [HumanMessage(content="Generate search queries that will help with planning the sections of the report.")])
@@ -119,7 +119,7 @@ async def generate_report_plan(state: ReportState, config: RunnableConfig):
             raise ValueError(f"Unsupported search API: {configurable.search_api}")
 
         # Format system instructions
-        system_instructions_sections = get_report_planner_instructions(report_type).format(topic=topic, report_organization=report_structure, context=source_str, feedback=feedback)
+        system_instructions_sections = report_planner_instructions.format(topic=topic, report_organization=report_structure, context=source_str, feedback=feedback)
 
         # Set the planner provider
         if isinstance(configurable.planner_provider, str):
@@ -189,7 +189,7 @@ def generate_queries(state: SectionState, config: RunnableConfig):
     structured_llm = writer_model.with_structured_output(Queries)
 
     # Format system instructions
-    system_instructions = get_query_writer_instructions(report_type).format(section_topic=section.description, number_of_queries=number_of_queries)
+    system_instructions = query_writer_instructions.format(section_topic=section.description, number_of_queries=number_of_queries)
 
     # Generate queries  
     queries = structured_llm.invoke([SystemMessage(content=system_instructions)]+[HumanMessage(content="Generate search queries on the provided topic.")])
@@ -239,7 +239,7 @@ def write_section(state: SectionState, config: RunnableConfig) -> Command[Litera
     configurable = Configuration.from_runnable_config(config)
 
     # Format system instructions
-    system_instructions = get_section_writer_instructions(report_type).format(section_title=section.name, section_topic=section.description, context=source_str, section_content=section.content)
+    system_instructions = section_writer_instructions.format(section_title=section.name, section_topic=section.description, context=source_str, section_content=section.content)
 
     # Generate section  
     section_content = writer_model.invoke([SystemMessage(content=system_instructions)]+[HumanMessage(content="Generate a report section based on the provided sources.")])
@@ -248,7 +248,7 @@ def write_section(state: SectionState, config: RunnableConfig) -> Command[Litera
     section.content = section_content.content
 
     # Grade prompt 
-    section_grader_instructions_formatted = get_section_grader_instructions(report_type).format(section_topic=section.description,section=section.content)
+    section_grader_instructions_formatted = section_grader_instructions.format(section_topic=section.description,section=section.content)
 
     # Feedback 
     structured_llm = writer_model.with_structured_output(Feedback)
